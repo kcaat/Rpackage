f578bd104d4638ec6f8ec8c3723697ca1f514dc9
kcaat <kat.lu@mail.utoronto.ca>
2023-11-30 14:47:10 -0500

    attempting to add code for simply dummy var

--- R/development.R
+++ R/development.R
@@ -1,3 +1,5 @@
+#before our presentation, send her our slides
+
 #library(available)
 library(devtools)
 library(usethis)
@@ -7,69 +9,81 @@ library(here)
 library(tidyverse)
 library(table1)
 
-available("dummypack", browse = FALSE)
-#available
+#available("dummypack", browse = FALSE)
+#name is available
 
 #load fake dataset
 data <- read.csv(here("data/randomdata.csv"))
 
 ### dummy vars with exclusive categories
 
-### multi-option dummy variables
+data$AreaType1 <- as.logical(data$AreaType1)
 
-
-
-# turn into factors, and then logicals
-cleandata <- function(dataset, statistic){
-  #Use the select() function in the dplyr package (part of tidyverse) subset the data to have   only the variables Country.Name, X2000 – X2019.
-  dataset <- dataset %>% select(Country.Name, X2000:X2019)
-
-  #remove prefix X from column names
-  colnames(dataset)<-gsub("X","",colnames(dataset))
-
-  #Use the pivot_longer() function to convert the data set from wide to long format.
-  dataset <- pivot_longer(dataset,
-                          cols=c('2000':'2019'),
-                          names_to = "Year", values_to = statistic,
-                          values_drop_na = FALSE,
-                          names_transform = list(Year = as.numeric))
-}
-
-#subset dummy variables based on name
-data_aspect <- data %>%
-  select(starts_with("Aspect"))
-
-print(sapply(data, class))
-
-#put names into list
-aspect_dummies <- colnames(data_aspect)
-
-data_aspect[aspect_dummies] <- lapply(data_aspect[aspect_dummies], factor)
-data_aspect[aspect_dummies] <- lapply(data_aspect[aspect_dummies], as.logical)
-
-data_aspect$Aspect1 <- as.logical(data_aspect$Aspect1)
-table(data_aspect$Aspect2)
-
-#label for table
-label(data$dummy1) <- "Discrim0 &mdash; n (%)"
-
-#render to only show 1s
 rndr <- function(x, ...) {
   y <- render.default(x, ...)
   if (is.logical(x)) y[2] else y
 }
 
-title1 <- "n (%)"
-title2 <- sprintf(data$Aspect1, "n (%)")
-title2 <- cat(data$Aspect1, "n (%)")
+tabledummy <- table1(~ AreaType1 + AreaType2 + AreaType3, data=data, render=rndr)
+tabledummy
 
-#use table1 to create frequency table
-table1(~ ., data=data_aspect, caption=title1, render=rndr)
+area_labels <- c("Urban", "Suburban", "Rural")
+
+subset <- data %>%
+  select(starts_with("AreaType"))
+
+dummies <- colnames(subset)
+
+label(data[, dummies]) <- area_labels
+
+#colnames(data) <- label_list
+data_labeled <- data %>%
+  mutate(across(starts_with("AreaType"), label<-area_labels))
+
+labeled_areas <- vector("list", length = length(area_labels))
+for (i in seq_along(area_labels)) {
+  labeled_areas[[i]] <- paste(area_labels[i], get(paste("area", i, sep = "")), sep = "_")
+}
+
+#normal labelling code
+label(data$AreaType1) <- "Urban"
+
+for (i in area_labels)
+
+#function
+dummycats <- function(data, prefix, label_list){
+
+  #subset data based on dummy prefix
+  subset <- data %>%
+    select(starts_with(prefix))
+
+  #rename variables based on labels list, if provided
+  if (!is.null(labels)) {
+    colnames(data) <- label_list
+  }
+
+  #turn dummy vars into logicals
+  data[colnames(data)] <- lapply(data[colnames(data)], as.logical)
+
+  #create render so that only YES is shown
+  rndr <- function(x, ...) {
+    y <- render.default(x, ...)
+    if (is.logical(x)) y[2] else y
+  }
+
+  #create title using prefix
+  title <- paste(prefix, "n (%)")
+
+  #create table1
+  return(table1(~ ., data=data, caption=title, render=rndr))
+}
+
+### multi-option dummy variables
 
 #list of labels
 aspect_names <- c("Health", "Education", "Transportation", "Social services", "Safety", "Diversity & inclusion", "Housing")
 
-### make function
+### function
 dummyfreq <- function(data, prefix, label_list){
 
   #subset data based on dummy prefix
